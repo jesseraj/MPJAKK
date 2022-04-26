6fdabe2698f01606df9da0358cb283b0508a54b4
jesseraj <89629604+jesseraj@users.noreply.github.com>
2022-04-26 11:20:43 +0300

    done

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import './App.css';
+import MediaTable from './components/MediaTable';
+
+const App = () => {
+  return <MediaTable />;
+};
+
+export default App;
---
+++ src/components/MediaTable.js
@@ -0,0 +1,35 @@
+// import PropTypes from 'prop-types';
+import {useEffect, useState} from 'react';
+import MediaRow from './MediaRow';
+
+const MediaTable = () => {
+  const [mediaArray, setMediaArray] = useState([]);
+  const getMedia = async () => {
+    try {
+      const response = await fetch('test.json');
+      const json = await response.json();
+      setMediaArray(json);
+    } catch (err) {
+      alert(err.message);
+    }
+  };
+
+  useEffect(() => {
+    getMedia();
+  }, []);
+
+  console.log(mediaArray);
+  return (
+    <table>
+      <tbody>
+        {mediaArray.map((item, index) => {
+          return <MediaRow key={index} file={item} />;
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+MediaTable.propTypes = {};
+
+export default MediaTable;
