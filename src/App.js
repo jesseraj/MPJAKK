1deb5917bd4972872c12070aa23c530daff8ddef
jesseraj <89629604+jesseraj@users.noreply.github.com>
2022-04-26 11:38:43 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css';
-import MediaTable from './components/MediaTable';
+import Nav from './components/Nav';
+import {MediaProvider} from './contexts/MediaContext';
+import Home from './views/Home';
+import Login from './views/Login';
+import Logout from './views/Logout';
+import Profile from './views/Profile';
+import Single from './views/Single';
 
 const App = () => {
-  return <MediaTable />;
+  return (
+    // eslint-disable-next-line no-undef
+    <Router basename={process.env.PUBLIC_URL}>
+      <MediaProvider>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/single" element={<Single />} />
+          <Route path="/logout" element={<Logout />} />
+        </Routes>
+      </MediaProvider>
+    </Router>
+  );
 };
 
 export default App;
---
+++ src/hooks/ApiHooks.js
@@ -0,0 +1,90 @@
+// TODO: add necessary imports
+import {useEffect, useState} from 'react';
+import {baseUrl} from '../utils/variables';
+
+const fetchJson = async (url, options = {}) => {
+  try {
+    const response = await fetch(url, options);
+    const json = await response.json();
+    if (response.ok) {
+      return json;
+    } else {
+      const message = json.message;
+      throw new Error(message);
+    }
+  } catch (err) {
+    throw new Error(err.message);
+  }
+};
+
+const useMedia = () => {
+  const [mediaArray, setMediaArray] = useState([]);
+  const getMedia = async () => {
+    try {
+      const media = await fetchJson(baseUrl + 'media');
+      const allFiles = await Promise.all(
+        media.map(async (file) => {
+          return await fetchJson(`${baseUrl}media/${file.file_id}`);
+        })
+      );
+      setMediaArray(allFiles);
+    } catch (err) {
+      alert(err.message);
+    }
+  };
+
+  useEffect(() => {
+    getMedia();
+  }, []);
+
+  return {mediaArray};
+};
+
+const useUser = () => {
+  const getUser = async (token) => {
+    const fetchOptions = {
+      headers: {
+        'x-access-token': token,
+      },
+    };
+    return await fetchJson(baseUrl + 'users/user', fetchOptions);
+  };
+
+  const getUsername = async (username) => {
+    const checkUser = await fetchJson(baseUrl + 'users/username/' + username);
+    if (checkUser.available) {
+      return true;
+    } else {
+      throw new Error('Username not available');
+    }
+  };
+
+  const postUser = async (inputs) => {
+    const fetchOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(inputs),
+    };
+    return await fetchJson(baseUrl + 'users', fetchOptions);
+  };
+
+  return {getUser, postUser, getUsername};
+};
+
+const useLogin = () => {
+  const postLogin = async (inputs) => {
+    const fetchOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(inputs),
+    };
+    return await fetchJson(baseUrl + 'login', fetchOptions);
+  };
+  return {postLogin};
+};
+
+export {useMedia, useLogin, useUser};
