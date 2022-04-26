0e7ce147a0cc62710a910f821b9b881862b5bea0
jesseraj <89629604+jesseraj@users.noreply.github.com>
2022-04-26 12:21:21 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,36 +1,20 @@
-import {Container} from '@mui/material';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css';
 import Nav from './components/Nav';
-import {MediaProvider} from './contexts/MediaContext';
 import Home from './views/Home';
-import Login from './views/Login';
-import Logout from './views/Logout';
 import Profile from './views/Profile';
 import Single from './views/Single';
-import {themeOptions} from './theme/themeOptions';
-import {ThemeProvider, createTheme} from '@mui/material/styles';
-
-const theme = createTheme(themeOptions);
 
 const App = () => {
   return (
     // eslint-disable-next-line no-undef
     <Router basename={process.env.PUBLIC_URL}>
-      <MediaProvider>
-        <ThemeProvider theme={theme}>
-          <Container maxWidth="lg">
-            <Nav />
-            <Routes>
-              <Route path="/" element={<Login />} />
-              <Route path="/home" element={<Home />} />
-              <Route path="/profile" element={<Profile />} />
-              <Route path="/single" element={<Single />} />
-              <Route path="/logout" element={<Logout />} />
-            </Routes>
-          </Container>
-        </ThemeProvider>
-      </MediaProvider>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/single" element={<Single />} />
+      </Routes>
     </Router>
   );
 };
--- src/views/Single.js
+++ src/views/Single.js
@@ -1,15 +1,5 @@
 import {useLocation} from 'react-router-dom';
 import {mediaUrl} from '../utils/variables';
-import {
-  Card,
-  CardContent,
-  CardMedia,
-  Typography,
-  List,
-  ListItem,
-  ListItemAvatar,
-  Avatar,
-} from '@mui/material';
 
 const Single = () => {
   const location = useLocation();
@@ -18,28 +8,8 @@ const Single = () => {
 
   return (
     <>
-      <Typography component="h1" variant="h2">
-        {file.title}
-      </Typography>
-      <Card>
-        <CardMedia
-          component="img"
-          image={mediaUrl + file.filename}
-          alt={file.title}
-          sx={{height: '60vh'}}
-        />
-        <CardContent>
-          <Typography>{file.description}</Typography>
-          <List>
-            <ListItem>
-              <ListItemAvatar>
-                <Avatar variant={'circle'} src={'logo192.png'} />
-              </ListItemAvatar>
-              <Typography variant="subtitle2">{file.user_id}</Typography>
-            </ListItem>
-          </List>
-        </CardContent>
-      </Card>
+      <h1>{file.title}</h1>
+      <img src={mediaUrl + file.filename} alt={file.title} />
     </>
   );
 };
